b0b1975f8a78cb190c9d35d74b3b7ccdcf89dc8b
Trevor256 <79394682+trevor256@users.noreply.github.com>
2023-02-04 16:44:49 -0600

    Create The_Apartment.py

---
+++ The_Apartment.py
@@ -0,0 +1,12 @@
+#Python textgame 
+
+print("Welcome to the worst game in the world")
+
+name = input("What is your name?: ")
+age = int(input("Your age?: "))
+gender = input("Your gender?: ")
+ 
+if age > 0:
+    print("Hello", gender, name,)
+else:
+    print("You literally arn't even alive lmao")
